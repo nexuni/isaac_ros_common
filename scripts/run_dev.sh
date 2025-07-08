da0694337dbe6fe2ffe33e0a1b3a5509df20b123
Nova Orin <nova.orin@mit.edu>
2025-07-08 11:26:38 +0800

    nxbot 3.2

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -251,7 +251,7 @@ if [[ $PLATFORM == "aarch64" ]]; then
     DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
     DOCKER_ARGS+=("--pid=host")
     DOCKER_ARGS+=("-v /usr/share/vpi3:/usr/share/vpi3")
-    DOCKER_ARGS+=("-v /dev/input:/dev/input")
+    DOCKER_ARGS+=("-v /dev:/dev")
 
     # If jtop present, give the container access
     if [[ $(getent group jtop) ]]; then
