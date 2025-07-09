2e2a865f52222fdb8b7b7415b91cb6df41f5e2e9
Nova Orin <nova.orin@mit.edu>
2025-07-09 14:22:03 +0800

    update lidar dev

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -251,7 +251,8 @@ if [[ $PLATFORM == "aarch64" ]]; then
     DOCKER_ARGS+=("-v /usr/src/jetson_multimedia_api:/usr/src/jetson_multimedia_api")
     DOCKER_ARGS+=("--pid=host")
     DOCKER_ARGS+=("-v /usr/share/vpi3:/usr/share/vpi3")
-    DOCKER_ARGS+=("-v /dev:/dev")
+    DOCKER_ARGS+=("-v /dev/input:/dev/input")
+    DOCKER_ARGS+=("-v /dev/ttyUSB0:/dev/ttyUSB0")
 
     # If jtop present, give the container access
     if [[ $(getent group jtop) ]]; then
